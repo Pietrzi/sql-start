e80b31f8fa02bf71dd86ee52c1138d8f9612536c
Pietrzi <pietrzi@op.pl>
2020-02-07 11:47:46 +0100

    more CRUD commands

--- first.sql
+++ first.sql
@@ -82,3 +82,141 @@ DROP TABLE people;
 SELECT * FROM people; 
 
 --show tables; 
+
+
+--MySQL Warnings Code
+--DESC cats; 
+
+--Try Inserting a cat with a super long name:
+
+INSERT INTO cats(name, age)
+VALUES('This is some text blah blah blah blah blah text text text something about cats lalalalal meowwwwwwwwwww', 10);
+--Then view the warning:
+
+--SHOW WARNINGS; 
+
+--Try inserting a cat with incorrect data types:
+
+INSERT INTO cats(name, age) VALUES('Lima', 'dsfasdfdas'); 
+
+--Then view the warning:
+
+--SHOW WARNINGS; 
+
+
+
+--NULL and NOT NULL Code
+--Try inserting a cat without an age:
+
+INSERT INTO cats(name) VALUES('Alabama'); 
+
+SELECT * FROM cats; 
+
+--Try inserting a nameless and ageless cat:
+
+--INSERT INTO cats() VALUES(); --
+
+
+
+--CODE: Setting Default Values 
+--Define a table with a DEFAULT name specified:
+
+CREATE TABLE cats3
+  (
+    name VARCHAR(20) DEFAULT 'no name provided',
+    age INT DEFAULT 99
+  );
+--Notice the change when you describe the table:
+
+--DESC cats3; 
+
+--Insert a cat without a name:
+
+INSERT INTO cats3(age) VALUES(13); 
+
+--Or a nameless, ageless cat:
+
+--INSERT INTO cats3() VALUES(); --
+
+--Combine NOT NULL and DEFAULT:
+
+CREATE TABLE cats4
+  (
+    name VARCHAR(20) NOT NULL DEFAULT 'unnamed',
+    age INT NOT NULL DEFAULT 99
+  );
+  
+--Notice The Difference:
+
+--INSERT INTO cats() VALUES();--
+ 
+SELECT * FROM cats;
+ 
+--INSERT INTO cats3() VALUES();--
+ 
+SELECT * FROM cats3;
+ 
+INSERT INTO cats3(name, age) VALUES('Montana', NULL);
+ 
+SELECT * FROM cats3;
+ 
+INSERT INTO cats4(name, age) VALUES('Cali', NULL);
+
+
+
+--CODE: Primary Keys
+--Define a table with a PRIMARY KEY constraint:
+
+CREATE TABLE unique_cats
+  (
+    cat_id INT NOT NULL,
+    name VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+  );
+--DESC unique_cats; --
+
+--Insert some new cats:
+
+INSERT INTO unique_cats(cat_id, name, age) VALUES(1, 'Fred', 23);
+ 
+INSERT INTO unique_cats(cat_id, name, age) VALUES(2, 'Louise', 3);
+ 
+INSERT INTO unique_cats(cat_id, name, age) VALUES(1, 'James', 3);
+--Notice what happens:
+
+SELECT * FROM unique_cats; 
+
+--Adding in AUTO_INCREMENT:
+
+CREATE TABLE unique_cats2 (
+    --cat_id INT NOT NULL AUTO_INCREMENT,--
+    name VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+);
+--INSERT a couple new cats:
+
+INSERT INTO unique_cats2(name, age) VALUES('Skippy', 4);
+INSERT INTO unique_cats2(name, age) VALUES('Jiff', 3);
+INSERT INTO unique_cats2(name, age) VALUES('Jiff', 3);
+INSERT INTO unique_cats2(name, age) VALUES('Jiff', 3);
+INSERT INTO unique_cats2(name, age) VALUES('Skippy', 4);
+--Notice the difference:
+
+SELECT * FROM unique_cats2; 
+
+
+
+--Table Constraints Exercise Solution
+--Defining The employees table:
+
+CREATE TABLE employees (
+    --id INT AUTO_INCREMENT NOT NULL,--
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INT NOT NULL,
+    current_status VARCHAR(255) NOT NULL DEFAULT 'employed',
+    PRIMARY KEY(id)
+);
