d1320d2db0c0cd978a6922b20d7a3beadb05fd2b
Pietrzi <pietrzi@op.pl>
2020-02-11 21:04:40 +0100

    more string functions

--- second.sql
+++ second.sql
@@ -198,3 +198,69 @@ SELECT REVERSE(author_fname) FROM books;
 SELECT CONCAT('woof', REVERSE('woof'));
  
 SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
+
+
+
+SELECT CHAR_LENGTH('Hello World');
+ 
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
+ 
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
+
+
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
+
+
+
+SELECT REVERSE(UPPER('Why does my cat look at me with such hatred?')); 
+
+
+SELECT UPPER(REVERSE('Why does my cat look at me with such hatred?')); 
+
+
+I-like-cats 
+
+
+SELECT REPLACE(CONCAT('I', ' ', 'like', ' ', 'cats'), ' ', '-'); 
+
+
+SELECT REPLACE(title, ' ', '->') AS title FROM books; 
+
+
+
+SELECT 
+   author_lname AS forwards,
+   REVERSE(author_lname) AS backwards 
+FROM books;
+
+
+SELECT
+   UPPER
+   (
+      CONCAT(author_fname, ' ', author_lname)
+   ) AS 'full name in caps'
+FROM books;
+
+
+SELECT
+   CONCAT(title, ' was released in ', released_year) AS blurb
+FROM books;
+SELECT
+   title,
+   CHAR_LENGTH(title) AS 'character count'
+FROM books;
+
+
+SELECT
+   CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title',
+   CONCAT(author_lname, ',', author_fname) AS author,
+   CONCAT(stock_quantity, ' in stock') AS quantity
+FROM books;
