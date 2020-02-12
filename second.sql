54b8bb0cd45ec82d1d4ae20c366b6372b2111094
Pietrzi <pietrzi@op.pl>
2020-02-12 18:34:04 +0100

    more examples

--- second.sql
+++ second.sql
@@ -366,3 +366,62 @@ SELECT title FROM books LIMIT 5;
 SELECT title FROM books LIMIT 5, 123219476457;
  
 SELECT title FROM books LIMIT 5, 50;
+
+
+-- like --
+
+SELECT title, author_fname FROM books WHERE author_fname LIKE '%da%';
+ 
+SELECT title, author_fname FROM books WHERE author_fname LIKE 'da%';
+ 
+SELECT title FROM books WHERE  title LIKE 'the';
+ 
+SELECT title FROM books WHERE  title LIKE '%the';
+ 
+SELECT title FROM books WHERE title LIKE '%the%';
+
+
+
+SELECT title, stock_quantity FROM books;
+ 
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '____';
+ 
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '__';
+ 
+(235)234-0987 LIKE '(___)___-____'
+ 
+SELECT title FROM books;
+ 
+SELECT title FROM books WHERE title LIKE '%\%%'
+ 
+SELECT title FROM books WHERE title LIKE '%\_%'
+
+
+SELECT title FROM books WHERE title LIKE '%stories%';
+ 
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+ 
+SELECT 
+    CONCAT(title, ' - ', released_year) AS summary 
+FROM books ORDER BY released_year DESC LIMIT 3;
+ 
+SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+ 
+SELECT title, released_year, stock_quantity 
+FROM books ORDER BY stock_quantity LIMIT 3;
+ 
+SELECT title, author_lname 
+FROM books ORDER BY author_lname, title;
+ 
+SELECT title, author_lname 
+FROM books ORDER BY 2,1;
+ 
+SELECT
+    CONCAT(
+        'MY FAVORITE AUTHOR IS ',
+        UPPER(author_fname),
+        ' ',
+        UPPER(author_lname),
+        '!'
+    ) AS yell
+FROM books ORDER BY author_lname;
