85b2d94130d56ac7fe14db0e595dfdf182490d73
Pietrzi <pietrzi@op.pl>
2020-02-14 10:39:31 +0100

    avg, sum, min-max problems

--- third.sql
+++ third.sql
@@ -59,3 +59,129 @@ FROM books;
  
 SELECT MAX(pages), title
 FROM books;
+
+
+--- problem with min max  ---
+
+
+SELECT * FROM books 
+WHERE pages = (SELECT Min(pages) 
+                FROM books); 
+ 
+SELECT title, pages FROM books 
+WHERE pages = (SELECT Max(pages) 
+                FROM books); 
+ 
+SELECT title, pages FROM books 
+WHERE pages = (SELECT Min(pages) 
+                FROM books); 
+ 
+SELECT * FROM books 
+--ORDER BY pages ASC LIMIT 1;
+ 
+SELECT title, pages FROM books 
+--ORDER BY pages ASC LIMIT 1;
+ 
+SELECT * FROM books 
+--ORDER BY pages DESC LIMIT 1;
+
+--- with group by ---
+
+
+SELECT author_fname, 
+       author_lname, 
+       Min(released_year) 
+FROM   books 
+GROUP  BY author_lname, 
+          author_fname;
+ 
+SELECT
+  author_fname,
+  author_lname,
+  Max(pages)
+FROM books
+GROUP BY author_lname,
+         author_fname;
+ 
+SELECT
+  CONCAT(author_fname, ' ', author_lname) AS author,
+  MAX(pages) AS 'longest book'
+FROM books
+GROUP BY author_lname,
+         author_fname;
+
+
+--- sum function ---
+
+
+SELECT SUM(pages)
+FROM books;
+ 
+SELECT SUM(released_year) FROM books;
+ 
+SELECT author_fname,
+       author_lname,
+       Sum(pages)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
+ 
+SELECT author_fname,
+       author_lname,
+       Sum(released_year)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
+
+
+--- avg func ---
+
+
+SELECT AVG(released_year) 
+FROM books;
+ 
+SELECT AVG(pages) 
+FROM books;
+ 
+SELECT AVG(stock_quantity) 
+FROM books 
+GROUP BY released_year;
+ 
+SELECT released_year, AVG(stock_quantity) 
+FROM books 
+GROUP BY released_year;
+ 
+SELECT author_fname, author_lname, AVG(pages) FROM books
+GROUP BY author_lname, author_fname;
+
+
+
+
+SELECT COUNT(*) FROM books;
+ 
+SELECT COUNT(*) FROM books GROUP BY released_year;
+ 
+SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
+ 
+SELECT Sum(stock_quantity) FROM BOOKS;
+ 
+SELECT AVG(released_year) FROM books GROUP BY author_lname, author_fname;
+ 
+SELECT author_fname, author_lname, AVG(released_year) FROM books GROUP BY author_lname, author_fname;
+ 
+SELECT CONCAT(author_fname, ' ', author_lname) FROM books
+WHERE pages = (SELECT Max(pages) FROM books);
+ 
+SELECT CONCAT(author_fname, ' ', author_lname) FROM books
+--ORDER BY pages DESC LIMIT 1;
+ 
+SELECT pages, CONCAT(author_fname, ' ', author_lname) FROM books
+ORDER BY pages DESC;
+ 
+SELECT released_year AS year,
+    COUNT(*) AS '# of books',
+    AVG(pages) AS 'avg pages'
+FROM books
+    GROUP BY released_year;
