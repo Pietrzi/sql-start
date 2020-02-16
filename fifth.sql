946e894fb859788767bdf2da553249ff6161a086
Pietrzi <pietrzi@op.pl>
2020-02-16 20:01:17 +0100

    mistake, not saved properly

---
+++ fifth.sql
@@ -0,0 +1,72 @@
+----- logical operators -----
+
+--- not equal ---
+
+SELECT title FROM books WHERE released_year = 2017;
+ 
+SELECT title FROM books WHERE released_year != 2017;
+ 
+SELECT title, author_lname FROM books;
+ 
+SELECT title, author_lname FROM books WHERE author_lname = 'Harris';
+ 
+SELECT title, author_lname FROM books WHERE author_lname != 'Harris';
+
+
+--- not like ---
+
+SELECT title FROM books WHERE title LIKE 'W';
+ 
+SELECT title FROM books WHERE title LIKE 'W%';
+ 
+SELECT title FROM books WHERE title LIKE '%W%';
+ 
+SELECT title FROM books WHERE title LIKE 'W%';
+ 
+SELECT title FROM books WHERE title NOT LIKE 'W%';
+
+--- greater than ---
+
+
+SELECT title, released_year FROM books ORDER BY released_year;
+ 
+SELECT title, released_year FROM books 
+WHERE released_year > 2000 ORDER BY released_year;
+ 
+SELECT title, released_year FROM books 
+WHERE released_year >= 2000 ORDER BY released_year;
+ 
+SELECT title, stock_quantity FROM books;
+ 
+SELECT title, stock_quantity FROM books WHERE stock_quantity >= 100;
+ 
+-- SELECT 99 > 1;
+ 
+-- SELECT 99 > 567;
+ 
+100 > 5
+-- true
+ 
+-15 > 15
+-- false
+ 
+9 > -10
+-- true
+ 
+1 > 1
+-- false
+ 
+'a' > 'b'
+-- false
+ 
+'A' > 'a'
+-- false
+ 
+'A' >=  'a'
+-- true
+ 
+SELECT title, author_lname FROM books WHERE author_lname = 'Eggers';
+ 
+SELECT title, author_lname FROM books WHERE author_lname = 'eggers';
+ 
+SELECT title, author_lname FROM books WHERE author_lname = 'eGGers';
